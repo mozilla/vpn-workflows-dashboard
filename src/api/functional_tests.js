fd2b156807b78106c8af27e060715ec6a2e32372
Raphael Okafor Jr <rokafor@mozilla.com>
2022-11-21 02:33:44 -0500

    skeleton

--- src/api/functional_tests.js
+++ src/api/functional_tests.js
@@ -3,10 +3,7 @@ import moment from 'moment';
 
 async function _getSingleWorkflow(workflowId) {
     const singleWorkflowRunApi = `https://api.github.com/repos/mozilla-mobile/mozilla-vpn-client/actions/workflows/${workflowId}/runs?branch=main`    
-    let return_data = {
-        fetched: Date.now(),
-        workflow_runs: []
-    };
+    let return_data = [];
 
     try {
         const { data } = await axios.get(singleWorkflowRunApi)
@@ -42,7 +39,7 @@ async function _getSingleWorkflow(workflowId) {
                     workflow_url: workflow.workflow_url            
                 }
     
-                return_data.workflow_runs.push(workflow_run)
+                return_data.push(workflow_run)
 
 
             }
@@ -58,10 +55,7 @@ async function _getSingleWorkflow(workflowId) {
 
 async function _getWorkflowCheckRuns(checkSuiteId) {
     const workflowCheckRunsApi = `https://api.github.com/repos/mozilla-mobile/mozilla-vpn-client/check-suites/${checkSuiteId}/check-runs`    
-    let return_data = {
-        fetched: Date.now(),
-        check_runs: []
-    };
+    let return_data = [];
     
     try {
         const { data } = await axios.get(workflowCheckRunsApi)
@@ -79,7 +73,7 @@ async function _getWorkflowCheckRuns(checkSuiteId) {
                     annotations: check_run.output.annotations_count
                 }
     
-                return_data.check_runs.push(run)
+                return_data.push(run)
             }
         }        
     } catch(err){
@@ -92,10 +86,7 @@ async function _getWorkflowCheckRuns(checkSuiteId) {
 
 async function _getWorkflowCheckRunAnnotation(checkRunId) {
     const workflowCheckRunAnnotationApi = `https://api.github.com/repos/mozilla-mobile/mozilla-vpn-client/check-runs/${checkRunId}/annotations`    
-    let return_data = {
-        fetched: Date.now(),
-        run_annotations: []
-    };
+    let return_data = [];
     
     try {
         const { data } = await axios.get(workflowCheckRunAnnotationApi)        
@@ -108,7 +99,7 @@ async function _getWorkflowCheckRunAnnotation(checkRunId) {
                 end_line: run_annotation.end_line
             }
 
-            return_data.run_annotations.push(annotation)
+            return_data.push(annotation)
         }        
     } catch(err){
         console.log('Unable to fetch getSingleWorkflow', err);
@@ -119,66 +110,124 @@ async function _getWorkflowCheckRunAnnotation(checkRunId) {
 }
 
 /// full api
-export async function getFullTestWorkflowReport() {
+export const getFullTestWorkflowReport = async () => {
+    console.log('inside line 123');
     let return_data = {        
         fetched: moment().format().valueOf(),        
         workflow_runs_data: []
     };
 
-    if(await _getLocalData()){        
+    if(await _getLocalData()){
+        console.log('line 130');        
         const localData = await _getDataFromLocal()
         return_data.fetch_message = "Its too early to request again, clear expiretime";
         return_data.workflow_runs_data = localData.workflow_runs_data
         return return_data;
     }
 
-    if(await _checkForUpdate()){
-        const localData = await _getDataFromLocal() 
-        return_data.fetch_message = "No New Workflows Available";
-        return_data.workflow_runs_data = localData.workflow_runs_data
-        return return_data;
-    }
+    // console.log('line 136');
+    // if(await _checkForUpdate()){
+    //     console.log('line 139');
+    //     const localData = await _getDataFromLocal() 
+    //     return_data.fetch_message = "No New Workflows Available";
+    //     return_data.workflow_runs_data = localData.workflow_runs_data
+    //     return return_data;
+    // }
     
+    console.log('line 144');
     try {
         console.log('new updates detected get new data');
         // get latest workflow runs for main branch
         const workflows = await _getSingleWorkflow(5937682)
         // get checks (functional test matrix run) for each workflow run
-        for(let i=0; i < workflows.workflow_runs.length; i++){
+        for(let i=0; i < workflows.length; i++){
             let workflow = {
                 workflow_run: {},
-                test_runs: []
+                test_runs: [],                
+                passed_count: 0,
+                failed_count: 0,
+                flaked_count: 0
             }
-            workflow.workflow_run = workflows.workflow_runs[i];
+
+            workflow.workflow_run = workflows[i];
             const delayTime = Math.floor(Math.random() * 1000) + 200
-            console.log('line 189 delaytime', delayTime)
+            // console.log('line 189 delaytime', delayTime)
             _delay(delayTime)
+            
+            // console.log('line 154', workflow.workflow_run.check_suite_id);
             const checkRuns = await _getWorkflowCheckRuns(workflow.workflow_run.check_suite_id)
-            for(let j=0; j < checkRuns.check_runs.length; j++){
+            // console.log('line 156');
+            for(let j=0; j < checkRuns.length; j++){
+                // console.log('line 158');
                 let test = {
                     test_run: {},
-                    test_annotations: []
-                }
+                    test_annotations: [],
+                    test_flake_history: [],
+                    test_flaked_count: 0,
+                    test_failed_count: 0,
+                    test_passed_count: 0,
+                }            
 
-                test.test_run = checkRuns.check_runs[j]
+                // console.log('line 164', checkRuns[j]);
+                test.test_run = checkRuns[j]
+                test.test_name = checkRuns[j].name
+                test.test_started = checkRuns[j].started
+                test.test_completed = checkRuns[j].completed
                 if(test.test_run.annotations > 0){
+                    let flake = {
+                        flakes: 0,
+                        failures: 0,
+                        messages: [],
+                        start_line: [],
+                        end_line: [],
+                    }
+
+                    let message = {}
+
                     const delayTime = Math.floor(Math.random() * 1000) + 200
                     console.log('line 197 delaytime', delayTime)
                     _delay(delayTime)
                     const annotations = await _getWorkflowCheckRunAnnotation(test.test_run.id)
-                    test.test_annotations = annotations.run_annotations
-                    workflow.workflow_run.flake_count += annotations.run_annotations.length
+                    test.test_annotations = annotations
+                    workflow.flaked_count += annotations.length
+                    flake.test_name = test.test_run.test_name;
+
+                    for(let k = 0; k < annotations.length; k++){
+                        if(annotations[k].annotation_level === 'warning'){
+                            flake.flakes++
+                            message.info = annotations[k].message
+                            message.start_line = annotations[k].start_line
+                            message.end_line = annotations[k].end_line
+                            flake.messages.push(message)
+                        }
+                        
+                        if(annotations[k].annotation_level === 'failure'){
+                            flake.failures++
+                            message.info = annotations[k].message
+                            message.start_line = annotations[k].start_line
+                            message.end_line = annotations[k].end_line
+                            flake.messages.push(message)
+                        }
+
+                        test.test_flake_history.push(flake)
+                    }
                 }
 
                 workflow.test_runs.push(test)
-                if(checkRuns.check_runs[j].conclusion){
-                    workflow.workflow_run.passed_tests += 1
+
+                if(checkRuns[j].conclusion === "success"){
+                    workflow.passed_count += 1
+                    test.test_passed_count += 1
+                } else {
+                    workflow.failed_count += 1
+                    test.test_failed_count += 1
                 }
             }
             
             return_data.workflow_runs_data.push(workflow)
         }        
 
+        console.log('line 188');
     } catch(err){
         console.log('Unable to fetch getFullTestWorkflowReport', err);
         return_data.message = "Unable to fetch getFullTestWorkflowReport"        
@@ -193,41 +242,47 @@ export async function getFullTestWorkflowReport() {
     // }
     // const newArray = return_data.workflow_runs_data
     // let new_return_data = await _appendDataJson(return_data.workflow_runs_data)
-    
-    window.localStorage.setItem('expireTime', moment().add(45, 'm').valueOf())
+    console.log('line 203');
+    window.localStorage.setItem('expireTime', moment().add(30, 'm').valueOf())
     window.localStorage.setItem('workflows', JSON.stringify(return_data))
     return return_data;
 }
 
 async function _getLocalData() {
     // fetch data only within 45 min intervals for same machine
+    console.log('line 217');
     const existingExpireTime = window.localStorage.getItem('expireTime')
-    const fortyFiveMinsAgo = moment().subtract(45, 'minutes').valueOf()        
-
-    if(existingExpireTime && existingExpireTime > fortyFiveMinsAgo){
-        console.log('is outside of expire time, make new request');
+    const fortyFiveMinsAgo = moment().subtract(45, 'minutes').valueOf()
+    
+    if(!existingExpireTime){
+        console.log('no expiry time, make new request');
+        return false
+    } else if(existingExpireTime && existingExpireTime > fortyFiveMinsAgo){
+        console.log('is within of expire time, get from local');
         return true
-    }
-
-    console.log('not outside of expiry');
-    return false
-}
-
-async function _checkForUpdate() {
-    const rawOrigData = window.localStorage.getItem('workflows')
-
-    if(!rawOrigData){
+    } else {
+        console.log('linec226');
+        console.log('not outside of expiry');
         return false
     }
 
-    const origData = JSON.parse(rawOrigData)    
-    const apiData = await _getSingleWorkflow(5937682)
-
-    const firstOrigData = origData.workflow_runs_data[0].workflow_run.id
-    const firstApiData = apiData.workflow_runs[0].id
-
-    return firstApiData === firstOrigData
 }
+
+// async function _checkForUpdate() {
+//     const rawOrigData = window.localStorage.getItem('workflows')
+
+//     if(!rawOrigData){
+//         return false
+//     }
+
+//     const origData = JSON.parse(rawOrigData)    
+//     const apiData = await _getSingleWorkflow(5937682)
+
+//     const firstOrigData = origData.workflow_runs_data[0].workflow_run.id
+//     const firstApiData = apiData.workflow_runs[0].id
+
+//     return firstApiData === firstOrigData
+// }
 
 async function _getDataFromLocal() {
     const rawOrigData = window.localStorage.getItem('workflows')
@@ -236,7 +291,16 @@ async function _getDataFromLocal() {
         return false
     }
 
+    // console.log(JSON.parse(rawOrigData));
     return JSON.parse(rawOrigData)    
 }
+
+// const buildTestRun = async (arg) => {
+
+// }
+
+// const buildWorkflowRun = async (arg) => {
+
+// }
 
 const _delay = ms => new Promise(res => setTimeout(res, ms));
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,97 +1,111 @@
 import React, { useEffect, useState } from 'react'
-import moment from 'moment'
-import { LineChart, Line, PieChart, Pie, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import moment from 'moment';
 import { getFullTestWorkflowReport } from '../api/functional_tests'
-// import { Doughnut } from 'react-chartjs-2';
-// import { PieChart } from 'recharts';
-// import { getFullTestWorkflowReport } from '../api/functional_tests'
-// import ChartPie from '../components/PieChart'
-// import wfData from '../data/data.json'
-const data = [{name: 'Page A', uv: 500, pv: 2800, amt: 3400}, {name: 'Page B', uv: 100, pv: 700, amt: 3000}, {name: 'Page C', uv: 200, pv: 800, amt: 400},];
-  
+import HomeComponent from '../components/HomeComponent';
+// const data = [{name: 'Page A', uv: 500, pv: 2800, amt: 3400}, {name: 'Page B', uv: 100, pv: 700, amt: 3000}, {name: 'Page C', uv: 200, pv: 800, amt: 400}];
 
 const HomePage = () => {
     const [isLoading, setLoading] = useState(false)
+    const [isLoadingFlakes, setLoadingFlakes] = useState(false)
     const [latestWorkflowData, setLatestWorkflowData] = useState({})
-    // const [workflowData, setWorkflowData] = useState({})
-    // const [chartData, setChartData] = useState()
+    const [testHistory, setTestHistory] = useState({})
+    const [passPercentage, setPassPercentage] = useState(0)
+    const [chartData, setChartData] = useState()
 
     useEffect(() => {
-        setLoading(true)        
         getData()
-        setLoading(false)
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     
-    const getData = async () => {
-        console.log('herererer');
-        const workflowData = await getFullTestWorkflowReport()
-        // console.log(workflowData);
-        // console.log(workflowData.workflow_runs_data[0]);
-        // setWorkflowData(workflowData)
-        // console.log(workflowData.workflow_runs_data.map(data => ({ name: data.workflow_run.title, data: data.workflow_run.conclusion })));
-        // console.log(workflowData.workflow_runs_data.map(data => ({ name: data.workflow_run.title, data: data.workflow_run.conclusion })));
-        setLatestWorkflowData(workflowData.workflow_runs_data[0])
+    const getData = () => {
+        setLoading(true)
+        getFullTestWorkflowReport().then((workflowData) => {
+            // console.log(workflowData);
+            // console.log('herererer', workflowData.workflow_runs_data[0]);
+            // console.log(workflowData.workflow_runs_data.map((data, idx) => console.log(data)));
+            // console.log(workflowData.workflow_runs_data.map((data, idx) => ({ name: data.test_runs[idx].test_run.name, counts: data.passed_count })));
+            let cData = workflowData.workflow_runs_data.map((data, idx) => ({ name: `${moment(data.workflow_run.started).format('L')} - ${data.workflow_run.title}`, passed: data.passed_count, flakes: data.flaked_count, failures: data.failed_count, date: moment(data.workflow_run.started).format('L') }))
+
+            const passPerc = Math.floor((workflowData.workflow_runs_data[0].passed_count / workflowData.workflow_runs_data[0].test_runs.length) * 100)
+            setPassPercentage(passPerc)
+            buildTestRuns(workflowData.workflow_runs_data)
+            setChartData(cData.reverse())
+            setLatestWorkflowData(workflowData.workflow_runs_data[0])
+            setLoading(false)
+        }).catch(err => console.log(err))
+    }
+
+    const buildTestRuns = (workflows) => {
+        setLoadingFlakes(true)
+        let flakes = []
+        for(let i =0; i<workflows.length; i++){
+            const workflow = workflows[i]
+            if(workflow.flaked_count > 0 || workflow.failed_count > 0){
+                const test_runs = workflow.test_runs
+                for(let j=0; j<test_runs.length; j++){
+                    const test_run = test_runs[j]
+
+                    if(test_run.test_flake_history.length){
+                        // console.log(test_run);
+                        const testFlakeHistory = {
+                            test_commit: workflow.workflow_run.title,
+                            test_commit_actor: workflow.workflow_run.actor_name,
+                            test_name: test_run.test_name,
+                            test_failed_count: 0,
+                            test_flaked_count: 0,
+                            test_date: test_run.test_completed,
+                            tests_error_messages: test_run.test_annotations
+                        }
+        
+                        for(let k=0; k<test_run.test_annotations.length; k++){
+                            const annotation = test_run.test_annotations[k]
+                            // console.log(annotation.annotation_level);
+                            if(annotation.annotation_level === 'warning'){
+                                testFlakeHistory.test_flaked_count++
+                            } 
+                            
+                            if(annotation.annotation_level === 'failure') {
+                                testFlakeHistory.test_failed_count++
+                            }
+                        }
+
+                        flakes.push(testFlakeHistory)
+                    }
+                }
+            }
+        }
+
+        let combinedTests = flakes.reduce(function(acc, curr) {
+            let findIndex = acc.findIndex(function(item) {
+              return item.test_commit === curr.test_commit
+            })
+          
+            if (findIndex === -1) {
+              acc.push(curr)
+            } else {
+              acc[findIndex] = (Object.assign({}, acc[findIndex], curr))
+          
+            }
+          
+          
+            return acc;
+          }, [])
+
+        setTestHistory(combinedTests)
+        setLoadingFlakes(false)
     }
 
     return (
         <div>
             {
                 isLoading ? <div>Loading...</div> :
-                <div id="main-home">
-                    <div id="left">
-                        <div className="latest">
-                            <div className="run-info">
-                                <div>{latestWorkflowData.workflow_run.title}</div>
-                                <div>{moment(latestWorkflowData.workflow_run.created).format("MMM. Do YYYY")}</div>
-                                <div>{latestWorkflowData.workflow_run.actor_name}</div>
-                                <div className="count">
-                                    <div>{latestWorkflowData.test_runs.length}</div>
-                                    <div>test cases</div>
-                                </div>
-                            </div>
-                            <div className="chart">
-                                <div>                                
-                                    <PieChart width={250} height={250}>
-                                        <Pie
-                                            dataKey="pv"
-                                            isAnimationActive={false}
-                                            data={data}
-                                            cx="50%"
-                                            cy="50%"
-                                            outerRadius={80}
-                                            fill="#8884d8"
-                                            label
-                                        />
-                                        <Pie dataKey="pv" data={data} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
-                                        <Tooltip />
-                                    </PieChart>                                
-                                </div>
-                            </div>
-                        </div>
-                        <div className="test-list">
-                            {
-                                latestWorkflowData.test_runs.map((run) => {
-                                    return (
-                                        <div style={{ maring: '2px 0', padding: '5px', cursor: 'pointer', borderBottom: '1px solid' }} key={run.test_run.id}>{run.test_run.name}</div>
-                                    )
-                                })
-                            }
-                        </div>
-                    </div>
-                    <div id="right">
-                        <div>
-                            <h3>Trends</h3>
-                            <LineChart width={800} height={300} data={data}>
-                                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                                <CartesianGrid stroke="#ccc" />
-                                <XAxis dataKey="name" />
-                                <YAxis />
-                                <Tooltip />
-                            </LineChart>
-                        </div>
-                        <div></div>
-                    </div>
-                </div>
+                <HomeComponent 
+                    latestWorkflowData={latestWorkflowData} 
+                    chartData={chartData} 
+                    isLoadingFlakes={isLoadingFlakes} 
+                    testHistory={testHistory} 
+                    passPercentage={passPercentage}  />
             }
         </div>
     )

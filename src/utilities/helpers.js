bb011ed75d51697d96c3a13b724f170f58218298
mozrokafor <94625718+mozrokafor@users.noreply.github.com>
2022-12-11 03:19:43 -0500

    small change

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -7,9 +7,8 @@ import moment from "moment";
 export function calculateDuration({ start, end }) {
     const startTime = moment(start, "HH:mm:ss a");
     const endTime = moment(end, "HH:mm:ss a");
-  
-    const hrs = moment.utc(endTime.diff(startTime)).format("HH");
+      
     const min = moment.utc(endTime.diff(startTime)).format("mm");
     const sec = moment.utc(endTime.diff(startTime)).format("ss");
-    return `${min}:${sec}`
+    return `${min}m ${sec}s`
   }

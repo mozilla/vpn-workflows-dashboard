df427ca8717c93c1efb3d9404023278c47eec497
mozrokafor <94625718+mozrokafor@users.noreply.github.com>
2022-12-11 03:11:53 -0500

    update run tim format

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import moment from "moment";
 
 export function calculateDuration({ start, end }) {
@@ -7,5 +11,5 @@ export function calculateDuration({ start, end }) {
     const hrs = moment.utc(endTime.diff(startTime)).format("HH");
     const min = moment.utc(endTime.diff(startTime)).format("mm");
     const sec = moment.utc(endTime.diff(startTime)).format("ss");
-    return moment.duration(`${hrs}:${min}:${sec}`).humanize();
+    return `${min}:${sec}`
   }
